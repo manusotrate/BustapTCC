93e894e25109b5015885924a44d357893ad377b6
Leo-March <leonardomarcheti2@gmail.com>
2025-09-03 15:36:56 -0300

    Quase terminando a tela home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+  userName = 'Maicon';
+  balance = 'R$00,00';
 
   constructor() {}
 
+  onSchedulesClick() {
+    console.log('Horários clicked');
+  }
+
+  onHelpClick() {
+    console.log('Ajuda clicked');
+  }
+
+  onRechargeClick() {
+    console.log('Recarga clicked');
+  }
 }
