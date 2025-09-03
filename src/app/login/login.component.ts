40d9ec0efa54321d7726e446118471a90c7055c4
detriste <detriste2018@gmail.com>
2025-09-03 14:02:47 -0300

    login integrado com banco

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule, IonInput } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    HttpClientModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+
+  @ViewChild('cpfInput', { static: true }) cpfInput!: IonInput;
+  @ViewChild('senhaInput', { static: true }) senhaInput!: IonInput;
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  irParaHome() {
+    const cpf = this.cpfInput.value as string;
+    const senha = this.senhaInput.value as string;
+
+    if (!cpf || !senha) {
+      alert('Preencha CPF e Senha!');
+      return;
+    }
+
+    this.http.post<any>('http://localhost:4000/login', { cpf, senha })
+      .subscribe({
+        next: (res) => {
+          alert(res.mensagem);
+          this.router.navigate(['/home']); // ✅ redireciona se login for válido
+        },
+        error: (err) => {
+          console.error(err);
+          alert(err.error?.erro || 'Erro no login');
+        }
+      });
+  }
+}
